8e842b64c2c69e007c276e43a01c5a6d0cb96e38
Sebastián Solorzano <sol21826@uvg.edu.gt>
2025-04-10 19:09:15 -0600

    Diseño

--- Cifrados_Asimetricos/src/Pages/Home/home.jsx
+++ Cifrados_Asimetricos/src/Pages/Home/home.jsx
@@ -3,7 +3,7 @@ import FileCard from "../../Components/FileCard";
 function Home() {
   return (
     <div
-      style={{ backgroundColor: "#121212", height: "92.5vh", padding: "30px" }}
+      style={{ backgroundColor: "#121212", height: "auto", padding: "30px" }}
     >
       <FileCard />
     </div>
